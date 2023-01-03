d7c5f9e9922a723d321d4113986e722d8f0da4fb
Eric Lich <eric.lichtensztein@aleia.io>
2023-01-03 01:31:15 -0300

    add load more movies btn and logic

--- components/MoreMovies.tsx
+++ components/MoreMovies.tsx
@@ -9,15 +9,22 @@ import type { Movie } from "../utils/types";
 
 const MoreMovies = () => {
   const [moreMovies, setMoreMovies] = useState<Movie[]>([]);
+  const [pageToLoad, setPageToLoad] = useState<number>(2);
+
   useEffect(() => {
-    fetch(`${API_TRENDING_MOVIES_WEEK_PAGE}2`).then(async (res) => {
+    fetch(`${API_TRENDING_MOVIES_WEEK_PAGE}${pageToLoad}`).then(async (res) => {
       const { results: movies }: { results: Movie[] } = await res.json();
-      setMoreMovies(movies);
+      console.log(movies);
+      if (moreMovies.length === 0) {
+        setMoreMovies(movies);
+      } else {
+        setMoreMovies((prevMovies) => (prevMovies = [...prevMovies, ...movies]));
+      }
     });
-  }, []);
+  }, [pageToLoad]);
 
   return (
-    <section className="w-full mt-[150px]">
+    <section className="w-full mt-[150px] flex flex-col">
       <div className="w-full flex justify-between items-center mb-8">
         <h2 className="text-white  font-semibold text-3xl ">More movies</h2>
         <Link href={"/"} className="text-[#FED530] font-semibold">
@@ -34,7 +41,7 @@ const MoreMovies = () => {
                   alt={`${movie.title} poster image`}
                   width={300}
                   height={350}
-                  quality={60}
+                  quality={40}
                   className="object-cover w-full h-auto hover:scale-105 hover:opacity-100 duration-500 opacity-95"
                 />
                 <div className="absolute bottom-0 w-full h-[200px] bg-gradient-to-b from-transparent to-black px-3 pb-9 flex">
@@ -44,6 +51,14 @@ const MoreMovies = () => {
             </Link>
           ))}
       </div>
+      <button
+        className="self-center mt-6 text-yellow-500 w-20 h-20 bg-yellow-500 rounded-full flex justify-center items-center"
+        onClick={() => setPageToLoad((prev) => prev + 1)}
+      >
+        <div className="w-[90%] h-[90%] bg-black rounded-full flex justify-center items-center text-center text-5xl">
+          <span className="self-center -mt-2">+</span>
+        </div>
+      </button>
     </section>
   );
 };
